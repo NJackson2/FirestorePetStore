63ba2c659ba8634b662243e007bf8a99254b2e26
Nick Jackson <nickdjackson@msn.com>
2022-10-14 14:19:14 -0400

    all done

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ initializeApp ({
 
 const db = getFirestore()
 
+// create new customer collection
+
 // const newCustomer1 = {
 //     name: "Diane",
 //     phone: 3051357921,
@@ -20,6 +22,7 @@ const db = getFirestore()
 //     .then(doc => console.log('Customer added: ', doc.id))
 //     .catch(console.error)
 
+// create new pet collection
 
 // const newPet = {
 //     species: "Snake",
@@ -32,16 +35,57 @@ const db = getFirestore()
 //     .then(doc => console.log('Pet added: ', doc.id))
 //     .catch(console.error)
 
-const newProduct = {
-    name: 'Scratching post',
-    price: "$59.99",
-    SKU: 7690156,
-    yearStocked: 2022
-}
+// create new product collection 
 
-db.collection('products').add(newProduct)
-    .then(doc => console.log('Product added: ', doc.id))
-    .catch(console.error)
+// const newProduct = {
+//     name: 'Scratching post',
+//     price: "$59.99",
+//     SKU: 7690156,
+//     yearStocked: 2022
+// }
+
+// db.collection('products').add(newProduct)
+//     .then(doc => console.log('Product added: ', doc.id))
+//     .catch(console.error)
+
+
+// list all pets, products and customers
+
+// db.collection('products').get()
+//     .then(collection => {
+//     collection.docs.forEach(doc => {
+//         console.log(doc.id, doc.data())
+//     })
+// })
+
+// db.collection('pets').get()
+//     .then(collection => {
+//     collection.docs.forEach(doc => {
+//         console.log(doc.id, doc.data())
+//     })
+// })
+
+// db.collection('customers').get()
+//     .then(collection => {
+//     collection.docs.forEach(doc => {
+//         console.log(doc.id, doc.data())
+//     })
+// })
+
+
+// updating documents in collection
+
+// db.collection("pets").doc('Pp7m3YcTa5Ip5fpoZ6g0')
+//     .update({breed: "shorthair"})
+//     .then(() => console.log('Update Done'))
+//     .catch(console.error)
+
+// db.collection("products").doc('JBLzYbheRKCYgFHdYKRd')
+//     .update({price: "$99.99"})
+//     .then(() => console.log('Update Done'))
+//     .catch(console.error)
+
+
 
 
 
